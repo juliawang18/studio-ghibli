83bf23523e75ffe7905d9815c6ff9c6d7438ab83
Julia Wang <juliawang@julias-mbp.attlocal.net>
2019-03-09 21:08:14 -0800

    Updates

---
+++ precache-manifest.62e27ecfda7fe81b9aa1ba8ca1331521.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "09cea8810e50a84d82274caa441c3251",
+    "url": "/studio-ghibili/static/media/monoke.09cea881.jpg"
+  },
+  {
+    "revision": "4d8dc7e3135456d121e4",
+    "url": "/studio-ghibili/static/css/main.792b349e.chunk.css"
+  },
+  {
+    "revision": "f9e35421d9f4a7773ea5",
+    "url": "/studio-ghibili/static/js/runtime~main.dce8f235.js"
+  },
+  {
+    "revision": "910848031acfc75ccc0d",
+    "url": "/studio-ghibili/static/js/2.c736600d.chunk.js"
+  },
+  {
+    "revision": "eb32498da8dfe67221f72e10c13638d5",
+    "url": "/studio-ghibili/static/media/title.eb32498d.jpg"
+  },
+  {
+    "revision": "4e7a6437f880d29731eae5d8a4e6c3a0",
+    "url": "/studio-ghibili/static/media/castle.4e7a6437.jpg"
+  },
+  {
+    "revision": "67535497298e35edca0963160cbc100a",
+    "url": "/studio-ghibili/static/media/fireflies.67535497.jpg"
+  },
+  {
+    "revision": "7f84fb0772ced5888a1d7cb20aabbb5c",
+    "url": "/studio-ghibili/static/media/totoro.7f84fb07.jpg"
+  },
+  {
+    "revision": "89a84318de21bf9b40654c5e57dccc9f",
+    "url": "/studio-ghibili/static/media/kikis.89a84318.jpg"
+  },
+  {
+    "revision": "5a41ebdfab4b466591ce58824b14fc8d",
+    "url": "/studio-ghibili/static/media/yesterday.5a41ebdf.jpg"
+  },
+  {
+    "revision": "da31f8fe7e5e00bec986d86643441ac3",
+    "url": "/studio-ghibili/static/media/rosso.da31f8fe.jpg"
+  },
+  {
+    "revision": "cb430dc49debfd090536bd93ae5c89b5",
+    "url": "/studio-ghibili/static/media/pompoko.cb430dc4.jpg"
+  },
+  {
+    "revision": "c00ec55aecd3e7ab2dfc929302de8287",
+    "url": "/studio-ghibili/static/media/whisper.c00ec55a.jpg"
+  },
+  {
+    "revision": "4d8dc7e3135456d121e4",
+    "url": "/studio-ghibili/static/js/main.a02c96a4.chunk.js"
+  },
+  {
+    "revision": "6076f2ac87b9e5ad58ed924d3f905aad",
+    "url": "/studio-ghibili/static/media/yamadas.6076f2ac.jpg"
+  },
+  {
+    "revision": "6bff4e7c600dfd1c159715e779fae237",
+    "url": "/studio-ghibili/static/media/spiritedaway.6bff4e7c.jpg"
+  },
+  {
+    "revision": "a67933366662b55ae13d23bcbc81f526",
+    "url": "/studio-ghibili/static/media/catreturns.a6793336.jpg"
+  },
+  {
+    "revision": "394a454d91500447b757cd5eb7bd47ab",
+    "url": "/studio-ghibili/static/media/howls.394a454d.jpg"
+  },
+  {
+    "revision": "22865f20880fddfe1d7d8b2f76f3c193",
+    "url": "/studio-ghibili/static/media/earthsea.22865f20.jpg"
+  },
+  {
+    "revision": "69f511947da38c241c456555a9f6197e",
+    "url": "/studio-ghibili/static/media/ponyo.69f51194.jpg"
+  },
+  {
+    "revision": "a0ef4ef0a25d5d7fbf9f1d5bb76730fc",
+    "url": "/studio-ghibili/static/media/arrietty.a0ef4ef0.jpg"
+  },
+  {
+    "revision": "0168312491b13ddd80678b52548b13f0",
+    "url": "/studio-ghibili/static/media/poppy.01683124.jpg"
+  },
+  {
+    "revision": "12b91b94903abaebb5d199ba4cfe9fbd",
+    "url": "/studio-ghibili/static/media/thewindrises.12b91b94.jpg"
+  },
+  {
+    "revision": "339f6dd35fec40aba18253a1d16aae53",
+    "url": "/studio-ghibili/static/media/kaguya.339f6dd3.jpg"
+  },
+  {
+    "revision": "2b943213b1e95f70915b05c0bfccaa0d",
+    "url": "/studio-ghibili/static/media/whenmarnie.2b943213.jpg"
+  },
+  {
+    "revision": "98db2ba057a0edf285d82fda4a6fd02a",
+    "url": "/studio-ghibili/index.html"
+  }
+];
